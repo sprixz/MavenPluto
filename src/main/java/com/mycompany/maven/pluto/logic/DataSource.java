9c8282e7f4571cd7d5e23c0eba0f11826b346517
Varga Bence <Varga Bence@192.168.0.186>
2016-05-23 19:31:13 +0200

    DataSource

--- src/main/java/com/mycompany/maven/pluto/logic/DataSource.java
+++ src/main/java/com/mycompany/maven/pluto/logic/DataSource.java
@@ -5,10 +5,61 @@
  */
 package com.mycompany.maven.pluto.logic;
 
+import javax.persistence.Cache;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+//import pluto.logic.controllers.PlutoUserJpaController;
+//import pluto.logic.controllers.SubjectJpaController;
+
 /**
  *
  * @author Varga Bence
  */
 public class DataSource {
+    
+    /*private static DataSource dataSource = null;
+    private EntityManagerFactory emf;
+    private final Cache cache = getEntityManagerFactory().getCache();
+    
+    //private PlutoUserJpaController userController;
+    //private SubjectJpaController subjectController;
+    
+    
+     public static DataSource getInstance() {
+        if (dataSource == null) {
+            dataSource = new DataSource();
+            
+        }
+        return dataSource;
+    }
+     
+    private DataSource(){
+       // userController = new PlutoUserJpaController(getEntityManagerFactory());
+        //subjectController = new SubjectJpaController(getEntityManagerFactory());   
+    }
+
+    /*public PlutoUserJpaController getUserController() {
+        return userController;
+    }
+
+    public SubjectJpaController getSubjectController() {
+        return subjectController;
+    }*/
+    
+    
+     
+     
+    /*public final EntityManagerFactory getEntityManagerFactory() {
+        emf = Persistence.createEntityManagerFactory("PlutoPU");
+        return emf;
+    }
+
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+*/
+    
+   
+    
     
 }
